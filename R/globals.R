267bf4c1ecc7989b18c885992a90edf09ad96e0e
kirstinlyon <kirstin@mltwelve.com>
2025-10-04 14:13:28 +0200

    Add scripts for cleaning provincial data

--- R/globals.R
+++ R/globals.R
@@ -1,2 +1,3 @@
 utils::globalVariables(c("indicator", "age", "x1", "type", "year", "file",
-                         "sheet_name", "sheets_to_exclude"))
+                         "sheet_name", "sheets_to_exclude", "category",
+                         "x2", "statistic", "category_indicator", "value"))
--- R/read_sex_age_specific_file.R
+++ R/read_sex_age_specific_file.R
@@ -13,10 +13,31 @@
 #'
 read_sex_age_specific_file <- function(filename, sheets_to_exclude = "Notes"){
 
+   temp <- read_file(filename, sheets_to_exclude)
+
+   return(temp)
+
+}
+
+
+#' Generic reading function for Thembisa excel files
+#'
+#' @param filename path to the Thembisa excel file
+#' @param sheets_to_exclude sheets to exclude from reading, default is "Notes"
+#'
+#' @returns cleaned dataset
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'   read_file(filename, sheets_to_exclude)
+#' }
+#'
+read_file <- function(filename, sheets_to_exclude = "Notes"){
     sheet_names <- readxl::excel_sheets(filename)
     sheet_names <- dplyr::setdiff(readxl::excel_sheets(filename), sheets_to_exclude)
 
-    all_data <- sheet_names %>%
+    temp <- sheet_names %>%
         purrr::set_names() %>%  # keep sheet names as list names
         purrr::map(~ {
             df <- readxl::read_xlsx(filename, sheet = .x)      # step 1: read sheet
@@ -26,6 +47,42 @@ read_sex_age_specific_file <- function(filename, sheets_to_exclude = "Notes"){
         }) %>%
         dplyr::bind_rows()  %>%
         dplyr::rename(country_province = sheet_name) %>%
+        thembisaR::add_country_province_name()
+
+        return(temp)
+   #     dplyr::mutate(country_province_name = dplyr::case_when(
+#            country_province == "SA" ~ "South Africa",
+#            country_province == "EC" ~ "Eastern Cape",
+#            country_province == "FS" ~ "Free State",
+#            country_province == "GT" ~ "Gauteng",
+#            country_province == "KZ" ~ "KwaZulu-Natal",
+#            country_province == "LM" ~ "Limpopo",
+#            country_province == "MP" ~ "Mpumalanga",
+#            country_province == "NC" ~ "Northern Cape",
+#            country_province == "NW" ~ "North West",
+#            country_province == "WC" ~ "Western Cape",
+#            TRUE ~ country_province
+#        ))
+
+}
+
+#' Function to add full names of provinces and country to data
+#'
+#' @param data data with country_province column
+#'
+#' @returns cleaned dataset with country_province_name column
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'   add_country_province_name(data)
+#' }
+#'
+
+add_country_province_name <- function(data){
+
+    temp <- data %>%
+     #   dplyr::rename(country_province = sheet_name) %>%
         dplyr::mutate(country_province_name = dplyr::case_when(
             country_province == "SA" ~ "South Africa",
             country_province == "EC" ~ "Eastern Cape",
@@ -40,4 +97,10 @@ read_sex_age_specific_file <- function(filename, sheets_to_exclude = "Notes"){
             TRUE ~ country_province
         ))
 
+
+
 }
+
+
+
+
